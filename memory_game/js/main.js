d032907d30d19a7c87881d12bf0cc83291956aa8
Kate Brenner <katebrenner@Kates-MacBook-Pro.local>
2017-11-09 13:51:13 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,21 +3,22 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0]
-cardsInPlay.push (cardOne);
-//cardsInPlay;
-console.log("user flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push (cardTwo);
-//cardsInPlay;
-console.log("user flipped king");
-
-
-// can I just leave this?  Do I need a path for if there are not 2 cards in play?
-if (cardsInPlay.lenth === 2) {
-}
+var checkForMatch = function () {
 if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You Found a Match!")
+alert("You found a match!");
 } else {
-	alert("Try Again!")
+alert("Sorry, try again.");
 }
+}
+
+
+var flipCard = function(cardId) {
+console.log("User flipped " + cards[cardId])
+cardsInPlay.push(cards[cardId])
+if (cardsInPlay.length === 2) {
+checkForMatch()
+}
+
+}
+flipCard(0)
+flipCard(2)
